677444b2d58e930cb48e79c8ef99bdf46ea536db
EYARieans <contentey6@gmail.com>
2024-04-16 11:38:12 +0530

    -Created new Post mothod in Category controller to process adding new category

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
