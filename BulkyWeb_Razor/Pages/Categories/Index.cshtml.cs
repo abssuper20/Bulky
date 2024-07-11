cf70e93bf5df620007d51387093f8f143a34e90d
EYARieans <contentey6@gmail.com>
2024-07-11 15:25:27 +0530

    -Chapter 58 Display all categories

--- BulkyWeb_Razor/Pages/Categories/Index.cshtml.cs
+++ BulkyWeb_Razor/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BulkyWeb_Razor.Data;
+using BulkyWeb_Razor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,17 @@ namespace BulkyWeb_Razor.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> categories;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            categories = _db.Categories.ToList();
         }
     }
 }
