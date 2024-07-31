f2a52255cf32d04cb2a40279b1a52997cee28b89
EYARieans <contentey6@gmail.com>
2024-07-31 17:07:09 +0530

    -Chapter 104 Edit Product link in Datatable

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -4,13 +4,28 @@ $(document).ready(function () {
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url : '/admin/product/getall' },
-        "columns" : [
-            { data: 'title' },
-            { data: 'isbn' },
-            { data: 'listPrice' },
-            { data: 'author' },
-            { data: 'category.name' }
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'title', "width": "50%" },
+            { data: 'isbn', "width": "10%" },
+            { data: 'listPrice', "width": "20%" },
+            { data: 'author', "width": "10%" },
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return ` <div class="w-75 btn-group" role="group">
+                                <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2">
+                                    <i class="bi bi-pencil-square"></i> Edit
+                                </a>
+
+                                <a class="btn btn-danger mx-2">
+                                    <i class="bi bi-trash-fill"></i> Delete
+                                </a>
+                            </div>`
+                },
+                "width": "10%"
+            }
         ]
     });
 }
