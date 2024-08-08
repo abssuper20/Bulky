f846d056b40062e7d67a0326b18247eaaf4cd24a
contentey6 <contentey6@gmail.com>
2024-08-08 16:37:42 +0530

    -Chapter 115 Extend Identity User

---
+++ Bulky.Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public int Name { get; set; }
+        public string? streetAddress { get; set; }
+        public string? city { get; set; }
+        public string? state { get; set; }
+        public string? postalCode { get; set; }
+    }
+}
